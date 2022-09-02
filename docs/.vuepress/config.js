bc9d125f82987795b1294060b2e8a877ba2bd34a
libo <2365850115@qq.com>
2022-09-02 09:31:29 +0800

    部分水果梳理完成，测试图片是否显示

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,54 @@
+module.exports = {
+    base: '/hongqiao/',
+    title: '小狗先森',
+    head: [ // 注入到当前页面的 HTML <head> 中的标签
+        ['link', { rel: 'icon', href: '/dog.png' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ],
+    themeConfig: {
+        logo: '/dog.png',
+        smoothScroll: true, //启用页面滚动效果
+        displayAllHeaders: true, // 默认值：false
+        nav:[ // 导航栏配置
+            {text: '首页', link: '/' },
+            {text: '指南', link: '/guide/' },
+            {text: '掘金', link: 'https://juejin.cn/user/3693991404250397' },
+            {text: 'Markdown', link: 'https://www.vuepress.cn/guide/markdown.html' },
+            {text: '部署', link: 'https://www.vuepress.cn/guide/deploy.html' },
+        ],
+        // 侧边栏
+        sidebar: {
+            '/recipes/': [
+                {
+                    title: '吃货清单',
+                    collapsable: false,
+                    children: [
+                        { title: '水果', path: '/recipes/fruits/'},
+                    ]
+                },
+                {
+                    title: '素',
+                    // collapsable: false,
+                    children: [
+                        { title: '土豆', path: '/recipes/vegetables/tudou'},
+                        { title: '番茄', path: '/recipes/vegetables/fanqie'},
+                    ]
+                },
+            ],
+            '/': [''],
+        },
+        sidebarDepth: 2,//左侧导航显示的层级
+        lastUpdated: 'Last Updated'
+    },
+    plugins: {
+        '@vssue/vuepress-plugin-vssue': {
+            platform: 'github-v4', //v3的platform是github，v4的是github-v4
+            locale: 'zh', //语言
+            // 其他的 Vssue 配置
+            owner: 'MrLi2000', //github账户名
+            repo: 'hongqiao', //github一个项目的名称
+            clientId: '0c1a015142f0a9696811',//注册的Client ID
+            clientSecret: '913080177981461bae4c4994dfc39dd8b09efedb',//注册的Client Secret
+            autoCreateIssue:true // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了。
+        },
+    },
+}
---
+++ docs/README.md
@@ -0,0 +1,11 @@
+---
+home: true
+heroImage: /dog.png
+heroText: 个人博客
+tagline: '欢迎来到小狗个人博客'
+actionText: 阅读指南→
+actionLink: /guide/
+---
+::: slot footer
+Copyright © 2022
+:::
